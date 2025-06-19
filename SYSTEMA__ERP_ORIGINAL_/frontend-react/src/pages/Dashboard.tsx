20ec2b45127b7f6cd027f566cc3d9434f49bc0a9
Jean Martins <137135776+JeanMartins0303@users.noreply.github.com>
2025-06-19 01:19:18 -0300

    main

--- SYSTEMA__ERP_ORIGINAL_/frontend-react/src/pages/Dashboard.tsx
+++ SYSTEMA__ERP_ORIGINAL_/frontend-react/src/pages/Dashboard.tsx
@@ -187,7 +187,7 @@ const Dashboard: React.FC = () => {
                 <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis dataKey="name" />
-                  <YAxis />
+                  <YAxis /> 
                   <Tooltip />
                   <Legend />
                   <Line 
